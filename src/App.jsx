a165ae1c64d218ca2ff592c358f17b0685138269
JIMI_BES <jimi.bes@gmail.com>
2023-03-28 17:07:44 +0200

    ajouts des autres pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,28 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
   
 return (
     <div>
-      <PokemonCard Pokemon={pokemonList[0]}/>
+      <PokemonCard Pokemon={pokemonList[2]}/>
     </div>
   )
 }
