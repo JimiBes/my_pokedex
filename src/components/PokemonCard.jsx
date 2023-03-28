380436a59c60fd97b57f13172bfa47562a0ca01f
JIMI_BES <jimi.bes@gmail.com>
2023-03-28 10:37:33 +0200

    création premiere carte pokemon

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+function PokemonCard() {
+    return (
+        <div>
+            <figure>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                    alt="A young Bulbasaur" />
+                <figcaption>Bulbasaur</figcaption>
+            </figure>
+        </div>
+    )
+}
+
+export default PokemonCard;
